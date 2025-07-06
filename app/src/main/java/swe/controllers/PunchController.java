ba3aeaea8619d834e413ca990f13317216447051
lajacl <lavette.clifton@gmail.com>
2025-07-06 15:27:11 -0400

    Add check for user current punch status

--- app/src/main/java/swe/controllers/PunchController.java
+++ app/src/main/java/swe/controllers/PunchController.java
@@ -1,5 +1,20 @@
 package swe.controllers;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import swe.Database;
+
 public class PunchController {
-    
+    public boolean isPunchedIn(int userId) throws SQLException {
+        Connection conn = Database.getConnection();
+        String sql = "SELECT * FROM punch_logs WHERE user_id = ? AND punch_out IS NULL;";
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.setInt(1, userId);
+
+        ResultSet rs = statement.executeQuery();
+        return rs.next();
+    }
 }
